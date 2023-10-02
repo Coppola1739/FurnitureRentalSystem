fbcd7d13df75ccf2c7c7c0c6624bd267b9704d09
gavcop <gavin.coppola@outlook.com>
2023-10-02 19:26:53 -0400

    initial commit

---
+++ src/main/java/edu/westga/cs3230/furniturerentalsystem/Main.java
@@ -0,0 +1,46 @@
+package edu.westga.cs3230.furniturerentalsystem;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Entry point for the collectible manager application
+ *
+ * @author Gavin Coppola
+ * @version Fall 2023
+ */
+public class Main extends Application {
+
+    private static final String WINDOW_TITLE = "Furniture Rental System";
+    private static final String GUI_FXML = "view/Login.fxml";
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(GUI_FXML));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle(WINDOW_TITLE);
+            primaryStage.show();
+        } catch (IllegalStateException | IOException anException) {
+            anException.printStackTrace();
+        }
+    }
+
+    /**
+     * Launches the application
+     *
+     * @param args not used
+     * @pre none
+     * @post none
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/main/java/edu/westga/cs3230/furniturerentalsystem/controller/LoginController.java
@@ -0,0 +1,46 @@
+package edu.westga.cs3230.furniturerentalsystem.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * Controller for the login page.
+ * 
+ * @author Gavin Coppola
+ * @version Fall 2023
+ */
+public class LoginController {
+
+	@FXML
+	private Button createAccountButton;
+	@FXML
+	private TextField user;
+	@FXML
+	private TextField password;
+
+	@FXML
+	private void validCredentials(ActionEvent event) throws IOException, SQLException {
+
+//		if (this.crossreferenceCredentials()) {
+//			this.changeScene(this.user, Utilities.AppConstants.MAIN_GUI_FXML);
+//		} else {
+//			this.alertError(this.user);
+//			this.alertError(this.password);
+//		}
+
+	}
+//
+//	private boolean crossreferenceCredentials() throws SQLException {
+//		return LoginManager.authorize(this.user.getText(), this.password.getText());
+//	}
+//
+	@FXML
+	void navigateToCreateAccountPage(ActionEvent event) throws IOException {
+//		this.changeScene(this.user, Utilities.AppConstants.CREATE_ACCOUNT_FXML);
+	}
+}
