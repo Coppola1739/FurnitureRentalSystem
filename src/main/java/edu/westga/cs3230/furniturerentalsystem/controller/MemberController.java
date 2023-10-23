af178a86d0ec25ec186aacab71a8055b28c3d31e
gavcop <gavin.coppola@outlook.com>
2023-10-22 21:09:54 -0400

    Implements landing page with member list

--- src/main/java/edu/westga/cs3230/furniturerentalsystem/controller/MemberController.java
+++ src/main/java/edu/westga/cs3230/furniturerentalsystem/controller/MemberController.java
@@ -1,10 +1,17 @@
 package edu.westga.cs3230.furniturerentalsystem.controller;
 
+import edu.westga.cs3230.furniturerentalsystem.dao.MemberDao;
+import edu.westga.cs3230.furniturerentalsystem.model.Member;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 
-public class MemberController {
+public class MemberController extends SystemController {
+    private MemberDao memberDao;
 
     @FXML
     private ScrollPane MemberScrollPane;
@@ -12,4 +19,35 @@ public class MemberController {
     @FXML
     private TextField MemberSearchTextField;
 
+    @FXML
+    private Label MemberUserNameLabel;
+
+    @FXML
+    private ListView<Member> MembersListView;
+
+    @FXML
+    void initialize() {
+        this.memberDao = new MemberDao();
+    }
+
+    @FXML
+    public void setLoggedInLabel(String username) {
+        this.MemberUserNameLabel.textProperty().set("Logged In: " + username);
+        this.loadMemberScrollPane();
+    }
+
+    private void loadMemberScrollPane() {
+        this.MembersListView.setItems(FXCollections.observableArrayList(this.memberDao.getAllMembers()));
+    }
+
+
+    @FXML
+    void editMember(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerMember(ActionEvent event) {
+
+    }
 }
---
+++ src/main/java/edu/westga/cs3230/furniturerentalsystem/controller/SystemController.java
@@ -0,0 +1,7 @@
+package edu.westga.cs3230.furniturerentalsystem.controller;
+
+public abstract class SystemController {
+
+    public void setLoggedInLabel(String text) {
+    }
+}
