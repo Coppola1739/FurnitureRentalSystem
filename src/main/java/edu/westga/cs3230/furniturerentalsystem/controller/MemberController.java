5b62b0951b79209f73883eb48db19abc7292ac5c
gavcop <gavin.coppola@outlook.com>
2023-10-22 21:44:50 -0400

    Adds log out button and functionality

--- src/main/java/edu/westga/cs3230/furniturerentalsystem/controller/MemberController.java
+++ src/main/java/edu/westga/cs3230/furniturerentalsystem/controller/MemberController.java
@@ -1,17 +1,35 @@
 package edu.westga.cs3230.furniturerentalsystem.controller;
 
+import edu.westga.cs3230.furniturerentalsystem.Main;
 import edu.westga.cs3230.furniturerentalsystem.dao.MemberDao;
 import edu.westga.cs3230.furniturerentalsystem.model.Member;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MemberController extends SystemController {
     private MemberDao memberDao;
+
+    @FXML
+    private Button EditMemberButton;
+
+    @FXML
+    private Button LogOutButton;
+
+    @FXML
+    private Button RegisterMemberButton;
 
     @FXML
     private ScrollPane MemberScrollPane;
@@ -50,4 +68,23 @@ public class MemberController extends SystemController {
     void registerMember(ActionEvent event) {
 
     }
+
+    @FXML
+    void logOut(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("/Login.fxml"));
+        loader.load();
+        Parent parent = loader.getRoot();
+        Scene scene = new Scene(parent);
+        Stage newStage = new Stage();
+
+        newStage.setScene(scene);
+        newStage.initModality(Modality.APPLICATION_MODAL);
+
+        newStage.show();
+        Stage stage = (Stage) this.LogOutButton.getScene().getWindow();
+
+        stage.close();
+    }
+
 }
