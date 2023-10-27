5d0590a9d9522bbbf6c4ce0667097b0a35e0b2a4
nsacand2 <nsacand2@my.westga.edu>
2023-10-27 13:50:38 -0400

    Added a page and controller for the furniture search.

---
+++ src/main/java/edu/westga/cs3230/furniturerentalsystem/controller/FurnitureController.java
@@ -0,0 +1,40 @@
+package edu.westga.cs3230.furniturerentalsystem.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class FurnitureController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private TextField furnitureIDSearchBox;
+
+    @FXML
+    private ListView<?> furnitureListView;
+
+    @FXML
+    private ComboBox<?> furnitureStyleDropdownBox;
+
+    @FXML
+    private ComboBox<?> furnitureTypeDropdownBox;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    void backToLandingPage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchFurniture(ActionEvent event) {
+
+    }
+
+}
