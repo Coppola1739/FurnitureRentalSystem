3e2908d38a981bad0e28c57438993f06c3c0735d
nsacand2 <nsacand2@my.westga.edu>
2023-11-08 15:35:37 -0500

    Refactored FurnitureDAO to remove string concatenation
    Also changed the way the search is conducted a bit to use constants.

--- src/main/java/edu/westga/cs3230/furniturerentalsystem/dao/FurnitureDao.java
+++ src/main/java/edu/westga/cs3230/furniturerentalsystem/dao/FurnitureDao.java
@@ -17,9 +17,10 @@ import edu.westga.cs3230.furniturerentalsystem.util.Constants;
 
 public class FurnitureDao {
 
-	private static final String CLOSING_QUOTATION = "'";
-
-	
+	public static final String FURNITURE_BY_STYLE = "SELECT * FROM `furniture` WHERE style_name like ?";
+	public static final String FURNITURE_BY_CATEGORY = "SELECT * FROM `furniture` WHERE category_name like ?";
+	public static final String FURNITURE_BY_ID = "SELECT * FROM `furniture` WHERE furniture_id like ?";
+	public static final String FURNITURE_BY_STYLE_AND_CATEGORY = "SELECT * FROM `furniture` WHERE style_name LIKE ? AND category_name LIKE ?";
 	
 	/**
 	 * Returns all furniture in the database
@@ -28,54 +29,6 @@ public class FurnitureDao {
 	
 	public ArrayList<Furniture> getAllFurniture() {
 		String selectFurniture = "SELECT * FROM `furniture`";
-		return this.searchQuery(selectFurniture);
-	}
-
-	
-	/**
-	 * Returns all furniture of the selected style 
-	 * @param style a string representing the style (e.g., Rustic, Scandinavian, etc.)
-	 * @return an ArrayList of furniture of the selected style
-	 */
-	
-	public ArrayList<Furniture> getFurnitureByStyle(String style) {
-		String selectFurniture = "SELECT * FROM `furniture` WHERE style_name like '" + style + CLOSING_QUOTATION;
-		return this.searchQuery(selectFurniture);
-	}
-
-	
-	/**
-	 * Returns all furniture of the selected category 
-	 * @param category a string representing the category(e.g., chair, table, etc)
-	 * @return an ArrayList of furniture of the selected category
-	 */
-	
-	public ArrayList<Furniture> getFurnitureByCategory(String category) {
-		String selectFurniture = "SELECT * FROM `furniture` WHERE category_name like '" + category + CLOSING_QUOTATION;
-		return this.searchQuery(selectFurniture);
-	}
-	
-	//TODO un-string-concatenate these methods
-	
-	
-	public ArrayList<Furniture> getFurnitureByStyleAndCategory(String style, String category) {
-		String selectFurniture = "SELECT * FROM `furniture` WHERE category_name LIKE '" + category + CLOSING_QUOTATION + " AND style_name LIKE '" + style + CLOSING_QUOTATION;
-		return this.searchQuery(selectFurniture);
-	}
-
-	
-	/**
-	 * Returns all furniture that have the entered string in their furniture ID
-	 * @param id the string to be searched for
-	 * @return an arrayList containing all furniture with @param id in their furniture ID
-	 */
-	
-	public ArrayList<Furniture> getFurnitureById(String id) {
-		String selectFurniture = "SELECT * FROM `furniture` WHERE furniture_id like '%" + id + "%'";
-		return this.searchQuery(selectFurniture);
-	}
-
-	private ArrayList<Furniture> searchQuery(String selectFurniture) {
 		ArrayList<Furniture> allFurniture = new ArrayList<>();
 		try (Connection connection = DriverManager.getConnection(Constants.CONNECTION_STRING);
 				PreparedStatement checkStmt = connection.prepareStatement(selectFurniture)) {
@@ -92,4 +45,91 @@ public class FurnitureDao {
 		}
 		return allFurniture;
 	}
+
+	
+	/**
+	 * Returns all furniture of the selected style 
+	 * @param style a string representing the style (e.g., Rustic, Scandinavian, etc.)
+	 * @return an ArrayList of furniture of the selected style
+	 */
+	
+	public ArrayList<Furniture> getFurnitureByStyle(String style) {
+		return this.searchQuery(FURNITURE_BY_STYLE, style);
+	}
+
+	
+	/**
+	 * Returns all furniture of the selected category 
+	 * @param category a string representing the category(e.g., chair, table, etc)
+	 * @return an ArrayList of furniture of the selected category
+	 */
+	
+	public ArrayList<Furniture> getFurnitureByCategory(String category) {
+		return this.searchQuery(FURNITURE_BY_CATEGORY, category);
+	}
+		
+	/**
+	 * Returns all furniture of the selected style and selected category 
+	 * @param category -a string representing the category(e.g., chair, table, etc)
+	 * @param style - a string representing the style (e.g., Rustic, Scandinavian, etc.)
+	 * @return an ArrayList of furniture of the selected category
+	 */
+	
+	public ArrayList<Furniture> getFurnitureByStyleAndCategory(String style, String category) {
+		return this.searchQuery(FURNITURE_BY_STYLE_AND_CATEGORY, style, category);
+	}
+
+	
+	/**
+	 * Returns all furniture that have the entered string in their furniture ID
+	 * @param id the string to be searched for
+	 * @return an arrayList containing all furniture with @param id in their furniture ID
+	 */
+	
+	public ArrayList<Furniture> getFurnitureById(String id) {
+		String modifiedId = "%" + id + "%";
+		return this.searchQuery(FURNITURE_BY_ID, modifiedId);
+	}
+
+	
+	private ArrayList<Furniture> searchQuery(String selectFurniture, String arg) {
+		ArrayList<Furniture> allFurniture = new ArrayList<>();
+		try (Connection connection = DriverManager.getConnection(Constants.CONNECTION_STRING);
+				PreparedStatement checkStmt = connection.prepareStatement(selectFurniture)) {
+				checkStmt.setString(1, arg);
+			try (ResultSet rs = checkStmt.executeQuery()) {
+				while (rs.next()) {
+					Furniture newFurniture = Furniture.builder().furnitureId(rs.getString("furniture_id"))
+							.styleName(rs.getString("style_name")).categoryName(rs.getString("category_name"))
+							.rentalRate(rs.getString("rental_rate")).build();
+					allFurniture.add(newFurniture);
+				}
+			}
+		} catch (SQLException exception) {
+			throw new RuntimeException(exception);
+		}
+		return allFurniture;
+	}
+	
+	private ArrayList<Furniture> searchQuery(String selectFurniture, String arg, String arg2) {
+		ArrayList<Furniture> allFurniture = new ArrayList<>();
+		try (Connection connection = DriverManager.getConnection(Constants.CONNECTION_STRING);
+				PreparedStatement checkStmt = connection.prepareStatement(selectFurniture)) {
+				checkStmt.setString(1, arg);
+				checkStmt.setString(2, arg2);
+			try (ResultSet rs = checkStmt.executeQuery()) {
+				while (rs.next()) {
+					Furniture newFurniture = Furniture.builder().furnitureId(rs.getString("furniture_id"))
+							.styleName(rs.getString("style_name")).categoryName(rs.getString("category_name"))
+							.rentalRate(rs.getString("rental_rate")).build();
+					allFurniture.add(newFurniture);
+				}
+			}
+		} catch (SQLException exception) {
+			throw new RuntimeException(exception);
+		}
+		return allFurniture;
+	}
+	
+	
 }
