afd0dee1c33ee3730fcb127deb973586bd982fe6
nsacand2 <nsacand2@my.westga.edu>
2023-11-08 15:35:37 -0500

    Refactored FurnitureDAO to remove string concatenation
    Also changed the way the search is conducted a bit to use constants.

--- src/main/java/edu/westga/cs3230/furniturerentalsystem/dao/FurnitureDao.java
+++ src/main/java/edu/westga/cs3230/furniturerentalsystem/dao/FurnitureDao.java
@@ -21,17 +21,17 @@ public class FurnitureDao {
 	public static final String FURNITURE_BY_CATEGORY = "SELECT * FROM `furniture` WHERE category_name like ?";
 	public static final String FURNITURE_BY_ID = "SELECT * FROM `furniture` WHERE furniture_id like ?";
 	public static final String FURNITURE_BY_STYLE_AND_CATEGORY = "SELECT * FROM `furniture` WHERE style_name LIKE ? AND category_name LIKE ?";
-	
+
 	/**
 	 * Returns all furniture in the database
 	 * @return an Arraylist with all furniture in the database
 	 */
-	
+
 	public ArrayList<Furniture> getAllFurniture() {
 		String selectFurniture = "SELECT * FROM `furniture`";
 		ArrayList<Furniture> allFurniture = new ArrayList<>();
 		try (Connection connection = DriverManager.getConnection(Constants.CONNECTION_STRING);
-				PreparedStatement checkStmt = connection.prepareStatement(selectFurniture)) {
+			 PreparedStatement checkStmt = connection.prepareStatement(selectFurniture)) {
 			try (ResultSet rs = checkStmt.executeQuery()) {
 				while (rs.next()) {
 					Furniture newFurniture = Furniture.builder().furnitureId(rs.getString("furniture_id"))
@@ -46,57 +46,57 @@ public class FurnitureDao {
 		return allFurniture;
 	}
 
-	
+
 	/**
-	 * Returns all furniture of the selected style 
+	 * Returns all furniture of the selected style
 	 * @param style a string representing the style (e.g., Rustic, Scandinavian, etc.)
 	 * @return an ArrayList of furniture of the selected style
 	 */
-	
+
 	public ArrayList<Furniture> getFurnitureByStyle(String style) {
 		return this.searchQuery(FURNITURE_BY_STYLE, style);
 	}
 
-	
+
 	/**
-	 * Returns all furniture of the selected category 
+	 * Returns all furniture of the selected category
 	 * @param category a string representing the category(e.g., chair, table, etc)
 	 * @return an ArrayList of furniture of the selected category
 	 */
-	
+
 	public ArrayList<Furniture> getFurnitureByCategory(String category) {
 		return this.searchQuery(FURNITURE_BY_CATEGORY, category);
 	}
-		
+
 	/**
-	 * Returns all furniture of the selected style and selected category 
+	 * Returns all furniture of the selected style and selected category
 	 * @param category -a string representing the category(e.g., chair, table, etc)
 	 * @param style - a string representing the style (e.g., Rustic, Scandinavian, etc.)
 	 * @return an ArrayList of furniture of the selected category
 	 */
-	
+
 	public ArrayList<Furniture> getFurnitureByStyleAndCategory(String style, String category) {
 		return this.searchQuery(FURNITURE_BY_STYLE_AND_CATEGORY, style, category);
 	}
 
-	
+
 	/**
 	 * Returns all furniture that have the entered string in their furniture ID
 	 * @param id the string to be searched for
 	 * @return an arrayList containing all furniture with @param id in their furniture ID
 	 */
-	
+
 	public ArrayList<Furniture> getFurnitureById(String id) {
 		String modifiedId = "%" + id + "%";
 		return this.searchQuery(FURNITURE_BY_ID, modifiedId);
 	}
 
-	
+
 	private ArrayList<Furniture> searchQuery(String selectFurniture, String arg) {
 		ArrayList<Furniture> allFurniture = new ArrayList<>();
 		try (Connection connection = DriverManager.getConnection(Constants.CONNECTION_STRING);
-				PreparedStatement checkStmt = connection.prepareStatement(selectFurniture)) {
-				checkStmt.setString(1, arg);
+			 PreparedStatement checkStmt = connection.prepareStatement(selectFurniture)) {
+			checkStmt.setString(1, arg);
 			try (ResultSet rs = checkStmt.executeQuery()) {
 				while (rs.next()) {
 					Furniture newFurniture = Furniture.builder().furnitureId(rs.getString("furniture_id"))
@@ -110,13 +110,13 @@ public class FurnitureDao {
 		}
 		return allFurniture;
 	}
-	
+
 	private ArrayList<Furniture> searchQuery(String selectFurniture, String arg, String arg2) {
 		ArrayList<Furniture> allFurniture = new ArrayList<>();
 		try (Connection connection = DriverManager.getConnection(Constants.CONNECTION_STRING);
-				PreparedStatement checkStmt = connection.prepareStatement(selectFurniture)) {
-				checkStmt.setString(1, arg);
-				checkStmt.setString(2, arg2);
+			 PreparedStatement checkStmt = connection.prepareStatement(selectFurniture)) {
+			checkStmt.setString(1, arg);
+			checkStmt.setString(2, arg2);
 			try (ResultSet rs = checkStmt.executeQuery()) {
 				while (rs.next()) {
 					Furniture newFurniture = Furniture.builder().furnitureId(rs.getString("furniture_id"))
@@ -130,6 +130,4 @@ public class FurnitureDao {
 		}
 		return allFurniture;
 	}
-	
-	
 }
